15ced89ab28be4fb43b0ef1f711922ce93cc1f84
Agustín <amarquardt@movizen.com>
2023-10-04 20:42:31 -0300

    upload user and movie mock up data

---
+++ app-movies/src/mock-data/movies.mock.ts
@@ -0,0 +1,39 @@
+export const MOCK_MOVIES = {
+  movies: [
+    {
+      id: 1,
+      title: 'Matrix',
+      description: 'Una película de ciencia ficción.',
+      year: 1999,
+      rate: 4.5,
+    },
+    {
+      id: 2,
+      title: 'Titanic',
+      description: 'Un drama romántico ambientado en un barco.',
+      year: 1997,
+      rate: 4.0,
+    },
+    {
+      id: 3,
+      title: 'Jurassic Park',
+      description: 'Aventura con dinosaurios.',
+      year: 1993,
+      rate: 4.2,
+    },
+    {
+      id: 4,
+      title: 'Star Wars: Episodio IV - Una Nueva Esperanza',
+      description: 'Una galaxia muy, muy lejana.',
+      year: 1977,
+      rate: 4.7,
+    },
+    {
+      id: 5,
+      title: 'Inception',
+      description: 'Un thriller de ciencia ficción.',
+      year: 2010,
+      rate: 4.4,
+    },
+  ],
+};
---
+++ app-movies/src/mock-data/users.mock.ts
@@ -0,0 +1,19 @@
+export const MOCK_USERS = {
+  users: [
+    {
+      id: 1,
+      username: 'John Doe',
+      password: '11223344', // cifrar las contraseñas en un entorno de producción
+    },
+    {
+      id: 2,
+      username: 'Jose Pepe',
+      password: '12345678',
+    },
+    {
+      id: 3,
+      username: 'Sergio Lopez',
+      password: '5667788',
+    },
+  ],
+};
