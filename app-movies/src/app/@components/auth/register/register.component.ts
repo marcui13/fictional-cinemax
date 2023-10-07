9ee32c0702adffb65abf9436f47a1dd45c5edeed
Agustín <amarquardt@movizen.com>
2023-10-07 20:25:09 -0300

    update register component

--- app-movies/src/app/@components/auth/register/register.component.ts
+++ app-movies/src/app/@components/auth/register/register.component.ts
@@ -1,9 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { NotificationService } from 'src/app/@services/notification.service';
+import { ApiService } from 'src/app/@services/api.service';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { IonicModule } from '@ionic/angular';
-import { NotificationService } from 'src/app/@services/notification.service';
 
 @Component({
   standalone: true,
@@ -18,12 +19,46 @@ export class RegisterComponent implements OnInit {
   email: string = '';
   password: string = '';
 
-  constructor() {}
+  constructor(
+    private apiService: ApiService,
+    private notificationService: NotificationService
+  ) {}
 
   ngOnInit() {}
 
   onSubmit() {
-    // Lógica para registrar al usuario
-    // Puedes implementar llamadas a servicios o API aquí
+    // Verifica que todos los campos estén completos
+    if (this.username && this.email && this.password) {
+      // Obtiene la lista actual de usuarios
+      const users = this.apiService.getUsers();
+
+      // Calcula el próximo ID disponible para el nuevo usuario
+      const nextUserId =
+        users.length > 0
+          ? Math.max(...users.map((user: any) => user.id)) + 1
+          : 1;
+
+      // Crea un nuevo objeto de usuario con los datos del formulario y el nuevo ID
+      const newUser = {
+        id: nextUserId,
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      };
+
+      // Llama al método para crear usuario en el ApiService
+      this.apiService.createUser(newUser);
+
+      // Notifica al usuario que se ha creado con éxito
+      this.notificationService.showSuccess('User created successfully', 'top');
+
+      // Restablece los campos del formulario
+      this.username = '';
+      this.email = '';
+      this.password = '';
+    } else {
+      // Muestra una advertencia si algún campo está incompleto
+      this.notificationService.showWarning('Complete all fields', 'top');
+    }
   }
 }
