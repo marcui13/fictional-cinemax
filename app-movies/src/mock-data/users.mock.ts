2fd54b281f411a9ae638ae4562bbf4ed2a6f753c
Agustín <amarquardt@movizen.com>
2023-10-07 20:24:16 -0300

    update user mock and user model

--- app-movies/src/mock-data/users.mock.ts
+++ app-movies/src/mock-data/users.mock.ts
@@ -3,16 +3,19 @@ export const MOCK_USERS = {
     {
       id: 1,
       username: 'John Doe',
+      email: 'jd@mail.com',
       password: '11223344', // cifrar las contraseñas en un entorno de producción
     },
     {
       id: 2,
       username: 'Jose Pepe',
+      email: 'pepe@mail.com',
       password: '12345678',
     },
     {
       id: 3,
       username: 'Sergio Lopez',
+      email: 'sergio@mail.com',
       password: '5667788',
     },
   ],
