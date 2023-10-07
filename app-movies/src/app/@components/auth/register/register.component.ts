73adee71613d006a64db4896607962cca171d886
Agustín <amarquardt@movizen.com>
2023-10-07 00:29:23 -0300

    update register component

--- app-movies/src/app/@components/auth/register/register.component.ts
+++ app-movies/src/app/@components/auth/register/register.component.ts
@@ -1,14 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { IonicModule } from '@ionic/angular';
+import { NotificationService } from 'src/app/@services/notification.service';
 
 @Component({
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
+  providers: [NotificationService, MatSnackBar],
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent  implements OnInit {
+export class RegisterComponent implements OnInit {
+  username: string = '';
+  email: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
 
+  onSubmit() {
+    // Lógica para registrar al usuario
+    // Puedes implementar llamadas a servicios o API aquí
+  }
 }
