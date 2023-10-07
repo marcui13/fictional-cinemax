5225065dfc76fdd854e2e0a68ea90b3093b345fe
Agustín <amarquardt@movizen.com>
2023-10-07 00:29:13 -0300

    update forgot-password component

--- app-movies/src/app/@components/auth/forgot-password/forgot-password.component.ts
+++ app-movies/src/app/@components/auth/forgot-password/forgot-password.component.ts
@@ -1,14 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { IonicModule } from '@ionic/angular';
+import { NotificationService } from 'src/app/@services/notification.service';
 
 @Component({
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
+  providers: [NotificationService, MatSnackBar],
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss'],
 })
-export class ForgotPasswordComponent  implements OnInit {
+export class ForgotPasswordComponent implements OnInit {
+  email: string = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
 
+  onSubmit() {
+    // Lógica para enviar solicitud de restablecimiento de contraseña
+    // Puedes implementar llamadas a servicios o API aquí
+  }
 }
