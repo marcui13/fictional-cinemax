e3f4f8e1b4f196ab5108b164c5e61e848c8d5545
Agustín <amarquardt@movizen.com>
2023-10-06 23:31:38 -0300

    update app.route

--- app-movies/src/app/app.routes.ts
+++ app-movies/src/app/app.routes.ts
@@ -2,12 +2,29 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'login',
+    loadComponent: () =>
+      import('./@components/auth/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+  {
+    path: 'movie-list',
+    loadComponent: () =>
+      import('./@components/movie/movie-list/movie-list.component').then(
+        (m) => m.MovieListComponent
+      ),
+  },
+  {
+    path: 'movie-detail',
+    loadComponent: () =>
+      import('./@components/movie/movie-detail/movie-detail.component').then(
+        (m) => m.MovieDetailComponent
+      ),
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
 ];
