505cdd3119762823d2948fdcd5ff7d4d6df1a85a
Agustín <amarquardt@movizen.com>
2023-10-07 20:24:37 -0300

    update login component

--- app-movies/src/app/@components/auth/login/login.component.ts
+++ app-movies/src/app/@components/auth/login/login.component.ts
@@ -5,8 +5,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { User } from 'src/app/@models/user.model';
+import { ApiService } from 'src/app/@services/api.service';
 import { NotificationService } from 'src/app/@services/notification.service';
-import { MOCK_USERS } from 'src/mock-data/users.mock';
 
 @Component({
   standalone: true,
@@ -23,29 +23,27 @@ export class LoginComponent {
   error: string = '';
 
   constructor(
+    private apiService: ApiService,
     private notificationService: NotificationService,
     private router: Router
   ) {}
 
   onLogin() {
-    const user = MOCK_USERS.users.find(
+    const users: User[] = this.apiService.getUsers();
+
+    // Utiliza find para buscar un usuario con el mismo username y password
+    const user = users.find(
       (u: User) => u.username === this.username && u.password === this.password
     );
 
-    if (this.username && this.password) {
-      if (user) {
-        // console.log('Usuario encontrado', user);
-        this.loginSuccess.emit(user); // Emite un evento de éxito de inicio de sesión
-        this.username = '';
-        this.password = '';
-        this.notificationService.showSuccess('Welcome!', 'top');
-        this.router.navigateByUrl('/movie-list');
-      } else {
-        // this.error = 'Credenciales incorrectas. Por favor, inténtalo de nuevo.';
-        this.notificationService.showError('Wrong credentials', 'top');
-      }
+    if (user) {
+      this.loginSuccess.emit(user); // Emite un evento de éxito de inicio de sesión
+      this.username = '';
+      this.password = '';
+      this.notificationService.showSuccess('Welcome!', 'top');
+      this.router.navigateByUrl('/movie-list');
     } else {
-      this.notificationService.showWarning('Complete all fields', 'top');
+      this.notificationService.showError('Invalid credentials', 'top');
     }
   }
 
