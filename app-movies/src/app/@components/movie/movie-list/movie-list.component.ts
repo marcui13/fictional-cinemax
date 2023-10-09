f934df67aeeeea94a2d937ed57b63609fc5631d7
Agustín <amarquardt@movizen.com>
2023-10-08 23:53:23 -0300

    update movie-list component

--- app-movies/src/app/@components/movie/movie-list/movie-list.component.ts
+++ app-movies/src/app/@components/movie/movie-list/movie-list.component.ts
@@ -2,9 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, LoadingController, MenuController } from '@ionic/angular';
 import { Movie } from 'src/app/@models/movie.model';
-import { MOCK_MOVIES } from 'src/mock-data/movies.mock';
+import { ApiService } from 'src/app/@services/api.service';
 
 @Component({
   standalone: true,
@@ -14,14 +14,50 @@ import { MOCK_MOVIES } from 'src/mock-data/movies.mock';
   styleUrls: ['./movie-list.component.scss'],
 })
 export class MovieListComponent implements OnInit {
-  movies: Movie[] = MOCK_MOVIES.movies;
+  movies: Movie[] = [];
   filteredMovies: Movie[] = [];
   showSearchBar: boolean = false;
   searchText: string = '';
+  closeItemOption: boolean = false;
+  loading: boolean = false;
+  loader: any;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private apiService: ApiService,
+    private menuController: MenuController,
+    private loadingController: LoadingController
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.movies = this.apiService.getMovies();
+  }
+
+  ionViewWillEnter() {
+    this.loading = true; // Activar el estado de carga
+
+    // Mostrar el spinner mientras se cargan los datos
+    this.presentLoading();
+
+    // Llama al método getMovies de ApiService para cargar las películas
+    this.movies = this.apiService.getMovies();
+
+    // Simula una carga de datos durante 3 segundos
+    setTimeout(() => {
+      this.loading = false; // Desactivar el estado de carga
+      if (this.loader) {
+        this.loader.dismiss(); // Ocultar el spinner si está visible
+      }
+    }, 1500);
+  }
+
+  async presentLoading() {
+    this.loader = await this.loadingController.create({
+      message: 'Loading...',
+      duration: 1500,
+    });
+    await this.loader.present();
+  }
 
   toggleSearch() {
     this.showSearchBar = !this.showSearchBar;
@@ -54,6 +90,7 @@ export class MovieListComponent implements OnInit {
     // Implementa la lógica de edición de la película aquí
     const movieIdString = movieId.toString();
     localStorage.setItem('movieId', movieIdString);
+    this.closeItemOption = true; // Cierra el ion-item-option
     this.router.navigate(['/movie-detail']);
   }
 
@@ -65,6 +102,7 @@ export class MovieListComponent implements OnInit {
   logout() {
     // Implementa la lógica para cerrar sesión
     // Por ejemplo, puedes eliminar el token de autenticación y redirigir a la página de inicio de sesión
+    this.menuController.close(); // Cierra el menú lateral
     this.router.navigateByUrl('/login');
   }
 
@@ -73,14 +111,8 @@ export class MovieListComponent implements OnInit {
     const starIcon = '★';
     const emptyIcon = '☆';
 
-    // Redondea la puntuación al entero más cercano
-    const roundedRating = Math.round(rating);
-
-    // Asegúrate de que la puntuación esté dentro del rango válido (0 a totalStars)
-    const clampedRating = Math.max(0, Math.min(totalStars, roundedRating));
-
-    const stars = starIcon.repeat(clampedRating);
-    const empty = emptyIcon.repeat(totalStars - clampedRating);
+    const stars = starIcon.repeat(rating);
+    const empty = emptyIcon.repeat(totalStars - rating);
 
     return stars + empty;
   }
