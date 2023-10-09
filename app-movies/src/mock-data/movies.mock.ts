b5c342c081e1a2a9e77659e61bbedc5c9dd8c7e4
Agustín <amarquardt@movizen.com>
2023-10-08 22:18:16 -0300

    update movies mock

--- app-movies/src/mock-data/movies.mock.ts
+++ app-movies/src/mock-data/movies.mock.ts
@@ -3,7 +3,8 @@ export const MOCK_MOVIES = {
     {
       id: 1,
       title: 'Matrix',
-      description: 'Una película de ciencia ficción.',
+      description:
+        'Una película de ciencia ficción que sigue la historia de Neo, un hacker que descubre que el mundo que conoce es una simulación controlada por máquinas inteligentes.',
       year: 1999,
       rate: 4.5,
       imagePath: './assets/movies/matrix.jpeg',
@@ -11,7 +12,8 @@ export const MOCK_MOVIES = {
     {
       id: 2,
       title: 'Titanic',
-      description: 'Un drama romántico ambientado en un barco.',
+      description:
+        'Un drama romántico ambientado en el famoso barco Titanic, que se hunde trágicamente en su viaje inaugural.',
       year: 1997,
       rate: 4.0,
       imagePath: './assets/movies/titanic.jpg',
@@ -19,7 +21,8 @@ export const MOCK_MOVIES = {
     {
       id: 3,
       title: 'Jurassic Park',
-      description: 'Aventura con dinosaurios.',
+      description:
+        'Una emocionante aventura que lleva a los espectadores a un parque temático lleno de dinosaurios clonados genéticamente que escapan y causan estragos.',
       year: 1993,
       rate: 4.2,
       imagePath: './assets/movies/jurassic.jpg',
@@ -27,7 +30,8 @@ export const MOCK_MOVIES = {
     {
       id: 4,
       title: 'Star Wars: Episodio IV - Una Nueva Esperanza',
-      description: 'Una galaxia muy, muy lejana.',
+      description:
+        'La primera entrega de la legendaria saga de Star Wars, donde Luke Skywalker se convierte en un héroe intergaláctico en una galaxia muy, muy lejana.',
       year: 1977,
       rate: 4.7,
       imagePath: './assets/movies/star-wars.jpg',
@@ -35,7 +39,8 @@ export const MOCK_MOVIES = {
     {
       id: 5,
       title: 'Inception',
-      description: 'Un thriller de ciencia ficción.',
+      description:
+        'Un thriller de ciencia ficción que explora la idea de robar secretos a través de los sueños. El protagonista, Dom Cobb, se sumerge en el mundo del espionaje corporativo a través de la manipulación de los sueños.',
       year: 2010,
       rate: 4.4,
       imagePath: './assets/movies/inception.jpg',
@@ -43,7 +48,8 @@ export const MOCK_MOVIES = {
     {
       id: 6,
       title: 'Pulp Fiction',
-      description: 'Una película de crimen y drama.',
+      description:
+        'Una película de crimen y drama dirigida por Quentin Tarantino que presenta múltiples historias interconectadas con personajes memorables y diálogos impactantes.',
       year: 1994,
       rate: 4.6,
       imagePath: './assets/movies/pulp-fiction.jpg',
@@ -51,7 +57,8 @@ export const MOCK_MOVIES = {
     {
       id: 7,
       title: 'The Shawshank Redemption',
-      description: 'Una película de drama y crimen en prisión.',
+      description:
+        'Una película de drama y crimen que sigue la vida de los reclusos en la prisión de Shawshank, centrándose en la amistad de dos hombres que buscan la redención.',
       year: 1994,
       rate: 4.7,
       imagePath: './assets/movies/shawshank-redemption.jpg',
@@ -59,7 +66,8 @@ export const MOCK_MOVIES = {
     {
       id: 8,
       title: 'The Godfather',
-      description: 'Un clásico del crimen y el drama.',
+      description:
+        'Un clásico del crimen y el drama que narra la historia de la familia Corleone y su involucramiento en el mundo del crimen organizado.',
       year: 1972,
       rate: 4.8,
       imagePath: './assets/movies/godfather.jpg',
@@ -67,7 +75,8 @@ export const MOCK_MOVIES = {
     {
       id: 9,
       title: 'Gladiator',
-      description: 'Una película de acción y drama histórico.',
+      description:
+        'Una película de acción y drama histórico que sigue la historia de Maximus Decimus Meridius, un general romano que busca venganza en el Coliseo de Roma.',
       year: 2000,
       rate: 4.4,
       imagePath: './assets/movies/gladiator.jpg',
@@ -75,7 +84,8 @@ export const MOCK_MOVIES = {
     {
       id: 10,
       title: 'The Dark Knight',
-      description: 'Una película de superhéroes y crimen.',
+      description:
+        'Una película de superhéroes y crimen que presenta al Caballero de la Noche, Batman, enfrentándose al Joker, un villano caótico que amenaza a Gotham City.',
       year: 2008,
       rate: 4.9,
       imagePath: './assets/movies/dark-knight.jpg',
@@ -83,7 +93,8 @@ export const MOCK_MOVIES = {
     {
       id: 11,
       title: 'Forrest Gump',
-      description: 'Un drama sobre la vida de Forrest Gump.',
+      description:
+        'Un drama sobre la vida de Forrest Gump, un hombre con discapacidades intelectuales que experimenta una serie de eventos históricos en Estados Unidos.',
       year: 1994,
       rate: 4.5,
       imagePath: './assets/movies/forrest-gump.jpg',
@@ -91,7 +102,8 @@ export const MOCK_MOVIES = {
     {
       id: 12,
       title: 'Fight Club',
-      description: 'Un drama psicológico y de acción.',
+      description:
+        'Un drama psicológico y de acción que sigue a un insomne y su relación con un carismático anarquista llamado Tyler Durden.',
       year: 1999,
       rate: 4.6,
       imagePath: './assets/movies/fight-club.jpg',
@@ -99,7 +111,8 @@ export const MOCK_MOVIES = {
     {
       id: 13,
       title: 'The Matrix Reloaded',
-      description: 'La secuela de Matrix.',
+      description:
+        'La secuela de Matrix que sigue a Neo en su lucha contra las máquinas mientras descubre más sobre la Matrix y su destino.',
       year: 2003,
       rate: 4.2,
       imagePath: './assets/movies/matrix-reloaded.jpg',
@@ -108,7 +121,7 @@ export const MOCK_MOVIES = {
       id: 14,
       title: 'Inglourious Basterds',
       description:
-        'Una película de guerra y drama dirigida por Quentin Tarantino.',
+        'Una película de guerra y drama dirigida por Quentin Tarantino que sigue a un grupo de soldados aliados que buscan vengarse de los nazis durante la Segunda Guerra Mundial.',
       year: 2009,
       rate: 4.4,
       imagePath: './assets/movies/inglourious-basterds.jpg',
@@ -117,7 +130,7 @@ export const MOCK_MOVIES = {
       id: 15,
       title: 'Avatar',
       description:
-        'Una película de ciencia ficción y aventuras en otro planeta.',
+        "Una película de ciencia ficción y aventuras que se desarrolla en el planeta Pandora y sigue la historia de Jake Sully, un ex-marine que se une a los Na'vi para proteger su hogar de la explotación.",
       year: 2009,
       rate: 4.3,
       imagePath: './assets/movies/avatar.jpg',
